ef51f9615f06f914d1852cdc281c9e8f8c14e059
Curtis <curtissalcedo@gmail.com>
2023-08-15 13:45:38 -0600

    apis working for create account, create profile

--- src/utilities/api/profiles-api.js
+++ src/utilities/api/profiles-api.js
@@ -1,5 +1,5 @@
 import sendRequest from "../send-request";
-const BASE_URL = '/api/profile';
+const BASE_URL = '/api/profiles';
 
 export async function createProfile(profileData) {
   return sendRequest(BASE_URL, 'POST', profileData);
