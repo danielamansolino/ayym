56db3f8a40c266aeeec3e62f2e15c896c52e61d0
Curtis <curtissalcedo@gmail.com>
2023-08-15 08:57:43 -0600

    small update to income

--- models/income.js
+++ models/income.js
@@ -5,6 +5,7 @@ const incomeSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user who received the income
   type: { type: String, required: true }, // Type of income (e.g., job, loan, side hustle)
   amount: { type: Number, required: true }, // Amount of the income
+  balance: { type: Number }, // Balance after the income
 }, {
   timestamps: true
 });
