b08409623671a40065c663ee33e3eb47908827ee
Curtis <curtissalcedo@gmail.com>
2023-08-14 19:26:37 -0600

    added models and controlllers for income, expense, goal

---
+++ models/goal.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const goalSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user who set the goal
+  title: { type: String, required: true }, // Title of the goal
+  targetAmount: { type: Number, required: true }, // Target amount for the goal
+  currentAmount: { type: Number, default: 0 }, // Current saved amount for the goal
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Expense', goalSchema);
---
+++ models/income.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const incomeSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user who received the income
+  type: { type: String, required: true }, // Type of income (e.g., job, loan, side hustle)
+  amount: { type: Number, required: true }, // Amount of the income
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Income', incomeSchema);
