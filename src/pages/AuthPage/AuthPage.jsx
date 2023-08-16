a7d13454c2c69959152d0fbea5f568b121ca8a3f
danielamansolino <danielamosqueragranda@gmail.com>
2023-08-16 10:14:08 -0400

    AuthPage update

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
 
+
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
+  
   return (
-    <main>
-      <h1>AuthPage</h1>
-      <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
-      { showSignUp ?
-          <SignUpForm setUser={setUser} />
-          :
-          <LoginForm setUser={setUser} />
-      }
+    <main className="auth-page-container">
+      <div className="auth-content">
+        <h1>Take charge of your money game like a pro with this budgeting app.</h1>
+        <p>Your ticket to financial success!</p>
+        <p>Already have an account? <span onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Sign In' : 'Log In'}</span></p>
+      </div>
+      
+      <div className="form-container">
+        {showSignUp ? <SignUpForm setUser={setUser} /> : <LoginForm setUser={setUser} />}
+      </div>
     </main>
   );
 }
